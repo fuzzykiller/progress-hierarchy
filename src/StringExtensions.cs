88df8699239277e44ef4fa5ed1a30e291b1d5e77
Daniel B <daniel.betz@daniel-betz.com>
2017-05-29 21:42:38 +0200

    Add StringExtensions: Surrogate-aware string manipulation
    In preparation of fancier progress bars

---
+++ src/StringExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ConsoleProgressBar
+{
+    /// <summary>
+    /// Contains extension methods on <see cref="String" />.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Similar to <see cref="string.PadRight(int, char)"/>, but aware of surrogate pairs.
+        /// </summary>
+        /// <param name="s">String to pad.</param>
+        /// <param name="length">Desired length.</param>
+        /// <param name="ps">Padding string, defaults to whitespace. Must represent single unicode code point.</param>
+        /// <returns>Padded string or original string if no padding was required.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="length"/> is negative.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="s"/> and/or <paramref name="ps"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="ps"/> did not represent single unicode codepoint.</exception>
+        public static string PadRightSurrogateAware(this string s, int length, string ps = " ")
+        {
+            if (s == null) throw new ArgumentNullException(nameof(s));
+            if (ps == null) throw new ArgumentNullException(nameof(ps));
+            if (length < 0) throw new ArgumentOutOfRangeException(nameof(length));
+            if (new StringInfo(ps).LengthInTextElements != 1) throw new ArgumentOutOfRangeException(nameof(ps), ps, "Padding string contains no or multiple code points.");
+
+            var currentLength = new StringInfo(s).LengthInTextElements;
+            var missingLength = length - currentLength;
+
+            if (missingLength < 1) return s;
+
+            var padding = string.Concat(Enumerable.Repeat(ps, missingLength));
+            return s + padding;
+        }
+
+        /// <summary>
+        /// Similar to <see cref="string.Substring(int, int)"/>, but aware of surrogate pairs.
+        /// </summary>
+        /// <param name="s">String to extract from.</param>
+        /// <param name="startIndex">Start at index.</param>
+        /// <param name="length">Number of characters to take.</param>
+        /// <returns>Extract from string.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     <paramref name="startIndex" /> and <paramref name="length" /> combine such that either end of the extract is out of bounds.
+        /// </exception>
+        /// <exception cref="ArgumentNullException"><paramref name="s"/> is  <c>null</c>.</exception>
+        public static string SubstringSurrogateAware(this string s, int startIndex, int length)
+        {
+            if (s == null) throw new ArgumentNullException(nameof(s));
+
+            var stringLength = new StringInfo(s).LengthInTextElements;
+            if (startIndex < 0 || startIndex > stringLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), startIndex, "Start index is not within bounds.");
+            }
+
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), length, "Length is negative.");
+            }
+
+            if (startIndex + length > stringLength)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), length, "Start index plus length are not within bounds.");
+            }
+
+            var substringTextElements = s.EnumerateTextElements().Skip(startIndex).Take(length);
+
+            return string.Concat(substringTextElements);
+        }
+
+        /// <summary>
+        /// Limit length of string to given limit, clipping parts as appropriate. Surrogate aware.
+        /// </summary>
+        /// <param name="s">String to limit in length.</param>
+        /// <param name="maxLength">Maximum length.</param>
+        /// <returns>Length-limited version of <paramref name="s"/> or <paramref name="s"/> if length was already ≤<paramref name="maxLength"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="s"/> is <c>null</c>.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="maxLength"/> is smaller than 0.</exception>
+        /// <remarks>Not an optimal solution, pending replacement</remarks>
+        public static string LimitLength(this string s, int maxLength)
+        {
+            if (s == null) throw new ArgumentNullException(nameof(s));
+            if (maxLength < 0) throw new ArgumentOutOfRangeException(nameof(maxLength));
+
+            var statusTextLength = new StringInfo(s).LengthInTextElements;
+            if (statusTextLength <= maxLength) return s;
+
+            if (maxLength > 12)
+            {
+                // Lots of space, leave some of the start
+                var part2Length = maxLength - 5;
+                var part2Start = statusTextLength - part2Length;
+
+                var part1 = s.SubstringSurrogateAware(0, 4) + "…";
+                var part2 = s.SubstringSurrogateAware(part2Start, part2Length);
+                
+                return part1 + part2;
+            }
+
+            if (maxLength > 5)
+            {
+                // Some space, show ellipsis at start
+                return "…" + s.SubstringSurrogateAware(statusTextLength - maxLength + 1, maxLength - 1);
+            }
+
+            return s.SubstringSurrogateAware(statusTextLength - maxLength, maxLength);
+        }
+
+        private static IEnumerable<string> EnumerateTextElements(this string s)
+        {
+            var enumerator = StringInfo.GetTextElementEnumerator(s);
+
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.GetTextElement();
+            }
+        }
+    }
+}
---
+++ test/StringExtensionsTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ConsoleProgressBar.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void PadRightSurrogateAware_throws_on_illegal_arguments()
+        {
+            Assert.That(() => StringExtensions.PadRightSurrogateAware(null, 1), Throws.ArgumentNullException, "'this' == null");
+            Assert.That(() => "".PadRightSurrogateAware(-1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'length' < 0");
+            Assert.That(() => "".PadRightSurrogateAware(1, null), Throws.ArgumentNullException, "'ps' == null");
+            Assert.That(() => "".PadRightSurrogateAware(1, ""), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'ps' = \"\"");
+            Assert.That(() => "".PadRightSurrogateAware(1, "  "), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'ps' == \"  \"");
+        }
+
+        [Test]
+        public void PadRightSurrogateAware_pads_shorter_strings()
+        {
+            Assert.That("".PadRightSurrogateAware(5), Is.EqualTo("     "));
+            Assert.That("".PadRightSurrogateAware(5, "a"), Is.EqualTo("aaaaa"));
+            Assert.That("".PadRightSurrogateAware(5, "🎄"), Is.EqualTo("🎄🎄🎄🎄🎄"));
+
+            Assert.That("a".PadRightSurrogateAware(5), Is.EqualTo("a    "));
+            Assert.That("a".PadRightSurrogateAware(5, "b"), Is.EqualTo("abbbb"));
+            Assert.That("a".PadRightSurrogateAware(5, "🎄"), Is.EqualTo("a🎄🎄🎄🎄"));
+
+            Assert.That("🎆".PadRightSurrogateAware(5), Is.EqualTo("🎆    "));
+            Assert.That("🎆".PadRightSurrogateAware(5, "b"), Is.EqualTo("🎆bbbb"));
+            Assert.That("🎆".PadRightSurrogateAware(5, "🎄"), Is.EqualTo("🎆🎄🎄🎄🎄"));
+
+            Assert.That("abc".PadRightSurrogateAware(4), Is.EqualTo("abc "));
+            Assert.That("a🎆🎄".PadRightSurrogateAware(4), Is.EqualTo("a🎆🎄 "));
+        }
+
+        [Test]
+        public void PadRightSurrageAware_leaves_longer_strings_alone()
+        {
+            Assert.That("Hello World".PadRightSurrogateAware(5), Is.EqualTo("Hello World"));
+            Assert.That("Hello".PadRightSurrogateAware(5), Is.EqualTo("Hello"));
+        }
+
+        [Test]
+        public void SubstringSurrogateAware_throws_on_illegal_arguments()
+        {
+            Assert.That(() => StringExtensions.SubstringSurrogateAware(null, 1, 1), Throws.ArgumentNullException, "'this' == null");
+            Assert.That(() => "".SubstringSurrogateAware(1, 1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'startIndex' > length");
+            Assert.That(() => "".SubstringSurrogateAware(0, 1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'startIndex' + 'length' > length");
+            Assert.That(() => "".SubstringSurrogateAware(0, -1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'length' < 0");
+
+            Assert.That(() => "a".SubstringSurrogateAware(0, 2), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'startIndex' + 'length' > length");
+            Assert.That(() => "a".SubstringSurrogateAware(2, 1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'startIndex' > length");
+
+            Assert.That(() => "🎆".SubstringSurrogateAware(0, 2), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'startIndex' + 'length' > length");
+            Assert.That(() => "🎆".SubstringSurrogateAware(2, 1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'startIndex' > length");
+        }
+
+        [Test]
+        public void SubStringSurrogateAware_extracts_correct_substring()
+        {
+            Assert.That("".SubstringSurrogateAware(0, 0), Is.EqualTo(""));
+
+            Assert.That("a".SubstringSurrogateAware(0, 0), Is.EqualTo(""));
+            Assert.That("a".SubstringSurrogateAware(0, 1), Is.EqualTo("a"));
+            Assert.That("a".SubstringSurrogateAware(1, 0), Is.EqualTo(""));
+            Assert.That("ab".SubstringSurrogateAware(0, 1), Is.EqualTo("a"));
+            Assert.That("ab".SubstringSurrogateAware(1, 1), Is.EqualTo("b"));
+            Assert.That("ab".SubstringSurrogateAware(0, 2), Is.EqualTo("ab"));
+
+            Assert.That("🎆".SubstringSurrogateAware(0, 0), Is.EqualTo(""));
+            Assert.That("🎆".SubstringSurrogateAware(0, 1), Is.EqualTo("🎆"));
+            Assert.That("🎆".SubstringSurrogateAware(1, 0), Is.EqualTo(""));
+            Assert.That("🎆🎄".SubstringSurrogateAware(0, 1), Is.EqualTo("🎆"));
+            Assert.That("🎆🎄".SubstringSurrogateAware(1, 1), Is.EqualTo("🎄"));
+            Assert.That("🎆🎄".SubstringSurrogateAware(0, 2), Is.EqualTo("🎆🎄"));
+            
+            Assert.That("🎆a🎄".SubstringSurrogateAware(0, 1), Is.EqualTo("🎆"));
+            Assert.That("🎆a🎄".SubstringSurrogateAware(1, 1), Is.EqualTo("a"));
+            Assert.That("🎆a🎄".SubstringSurrogateAware(0, 2), Is.EqualTo("🎆a"));
+            Assert.That("🎆a🎄".SubstringSurrogateAware(1, 2), Is.EqualTo("a🎄"));
+        }
+
+        [Test]
+        public void LimitLength_throws_on_illegal_arguments()
+        {
+            Assert.That(() => StringExtensions.LimitLength(null, 1), Throws.ArgumentNullException, "'this' == null");
+            Assert.That(() => "".LimitLength(-1), Throws.InstanceOf<ArgumentOutOfRangeException>(), "'length'  < 0");
+        }
+
+        [Test]
+        public void LimitLength_limits_longer_strings_correctly()
+        {
+            Assert.That("Hello World".LimitLength(6), Is.EqualTo("…World"));
+            Assert.That("Hello World".LimitLength(4), Is.EqualTo("orld"));
+            Assert.That("Hallo World 🎆".LimitLength(6), Is.EqualTo("…rld 🎆"));
+            Assert.That("Hello World".LimitLength(0), Is.EqualTo(""));
+            Assert.That("Slightly long-ish version of Hello World 🌟".LimitLength(14), Is.EqualTo("Slig…o World 🌟"));
+        }
+
+        [Test]
+        public void LimitLength_leaves_shorter_strings_alone()
+        {
+            Assert.That("Hello World".LimitLength(50), Is.EqualTo("Hello World"));
+            Assert.That("Hello World".LimitLength(11), Is.EqualTo("Hello World"));
+            Assert.That("Hello".LimitLength(5), Is.EqualTo("Hello"));
+            Assert.That("".LimitLength(0), Is.EqualTo(""));
+        }
+    }
+}
